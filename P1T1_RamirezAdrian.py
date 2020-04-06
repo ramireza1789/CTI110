ac23442b3573a439a7e5f7d285c7e84c58a98b9c
ramireza1789 <63216223+ramireza1789@users.noreply.github.com>
2020-04-05 21:32:09 -0400

    Add files via upload

---
+++ P1T1_RamirezAdrian.py
@@ -0,0 +1,5 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 22:45:29) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("This is a test of IDLE")
+This is a test of IDLE
+>>> 
